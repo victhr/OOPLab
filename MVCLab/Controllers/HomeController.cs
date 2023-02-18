8c0c047379daa13804143eac7d296f78dd47020c
VR <victhr@hotmail.com>
2023-02-18 09:35:47 -0600

    test push

--- MVCLab/Controllers/HomeController.cs
+++ MVCLab/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace MVCLab.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
